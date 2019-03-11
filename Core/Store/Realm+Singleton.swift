ad72e009b3a251812a68a7cb65d7267a9abc074f
Nazariy Gorpynyuk <nazariyg@gmail.com>
2019-03-11 15:44:52 +0200

    Update Realm+Singleton.swift

--- Core/Store/Realm+Singleton.swift
+++ Core/Store/Realm+Singleton.swift
@@ -2,35 +2,36 @@
 // All rights reserved.
 
 import Foundation
+import Cornerstones
 import RealmSwift
 
 public extension Realm {
 
     public subscript<Model: Object>(key: Model.Type) -> Model? {
         get {
-            return objects(key).first
+            let existingObjects = objects(key)
+            assert(existingObjects.count <= 1)
+            return existingObjects.first
         }
         set(object) {
-            if let object = object {
-                if let existingObject = objects(Model.self).first {
-                    if !object.isSameObject(as: existingObject) {
-                        // The two objects are distinct. Replace.
-                        modify {
-                            delete(existingObject)
-                            add(object)
-                        }
-                    }
-                } else {
-                    // Add.
+            // Relying on the fact that `key` is a singleton itself.
+            synchronized(key) {
+                if let object = object {
+                    // Add or replace.
+                    let existingObjects = objects(key)
                     modify {
+                        existingObjects.forEach { existingObject in
+                            if !existingObject.isSameObject(as: object) {
+                                delete(existingObject)
+                            }
+                        }
                         add(object)
                     }
-                }
-            } else {
-                // Delete if exists.
-                if let existingObject = objects(Model.self).first {
+                } else {
+                    // Delete.
+                    let existingObjects = objects(key)
                     modify {
-                        delete(existingObject)
+                        delete(existingObjects)
                     }
                 }
             }
